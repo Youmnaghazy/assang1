ba73d4b4bfaa7dc71c6876cfe0d5f6dda8ec822a
Youmnaghazy <Youmnaghazy2024@gmail.com>
2023-09-01 11:06:31 +0200

    angular1

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  imgSrc:string='assets/img/avataaars.svg';
+imgWidth:number=300;
+}
